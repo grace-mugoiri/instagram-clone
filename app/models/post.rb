d40b0d9c272c1a00696dac42eff8e415b8435e56
grace-mugoiri <gracemugoiri@gmail.com>
2020-11-04 12:20:20 +0300

    create hash tags for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,25 @@ class Post < ApplicationRecord
 	has_one_attached :image
 	has_many :post_hash_tags
 	has_many :hash_tags, through: :post_hash_tags
+	after_commit :create_hash_tags, on: :create
 
 	validate :image_presence
 
 	def image_presence
 		errors.add(:image, "can't be blank") unless image.attached?
+	end
+
+	def extract_name_hash_tags
+		description.to_s.scan(/#\w+/).map{|name| name.gsub("#", "")}
+	end
+
+	def create_hash_tags
+		extract_name_hash_tags do |name|
+			hash_tags.create(name: name)
+		end
+	end
+
+	def extract_name_hash_tags
+		description.to_s.scan(/#\w+/).map{|name| name.gsub("#", "")})
 	end
 end
